e1404bf40c5f925515f804ba05ccbff59f8e6dab
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-06 09:12:38 +0430

    Added star model

--- chikarestoon/api/models.py
+++ chikarestoon/api/models.py
@@ -8,3 +8,11 @@ class Idea(models.Model):
 
     def __str__(self):
         return self.content
+
+class Star(models.Model):
+    stars = models.BigIntegerField(default=0)
+    related_idea = models.ForeignKey(Idea,on_delete=models.CASCADE)
+
+class StarredPosts(models.Model):
+    starred_idea = models.ForeignKey(Idea,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
