6d01caa87e4dddfebc5b5d89b669e5627b5a5c9b
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-06 09:13:19 +0430

    Added api urls

--- chikarestoon/api/urls.py
+++ chikarestoon/api/urls.py
@@ -1,6 +1,20 @@
-from django.urls import path
-from .views import IdeaListView
+from django.urls import path,include
+
+#from .views import IdeaViewSet
+
+#urlpatterns = [
+#    path("",IdeaViewSet.as_view({'get':'get'}),name="list"),
+#    path("stars/",StarListView.as_view(),name="star-list"),
+#    path("stars/<int:pk>/",StarUpdateView.as_view(),name="star-update")
+#]
+
+from .views import IdeaViewSet,StarViewSet
+from rest_framework import routers
+router = routers.SimpleRouter()
+router.register(r'idea', IdeaViewSet,basename="idea")
 
 urlpatterns = [
-    path("",IdeaListView.as_view(),name="list")
+    path('star/<int:pk>/',StarViewSet.as_view({'post':'create','get':'retrieve'}))
 ]
+
+urlpatterns += router.urls
