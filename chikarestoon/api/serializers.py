b0ac7f5d095bcff4f3e54db7da7df1ac6252f8f3
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-06 09:13:03 +0430

    Added star serializer

--- chikarestoon/api/serializers.py
+++ chikarestoon/api/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
-from .models import Idea
+from .models import Idea,Star
 class IdeaSerialiser(serializers.ModelSerializer):
     class Meta:
         model = Idea
         fields = "__all__"
+
+class StarSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Star
+        fields = "__all__"
+    def validate_stars(self,value):
+        print(value)
+        return value
+
