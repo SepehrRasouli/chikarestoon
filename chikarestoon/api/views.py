0d93bb3e70432e6757bdf4905130af92aea63ae9
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-12 20:04:53 +0430

    Added views

--- chikarestoon/api/views.py
+++ chikarestoon/api/views.py
@@ -1,63 +1,112 @@
+import random
 from django.shortcuts import render,get_object_or_404
+from django.contrib.auth.models import User
+from django.http.response import Http404
 from rest_framework import viewsets,status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .models import Idea,Star,StarredPosts
-from .serializers import IdeaSerialiser,StarSerialiser
+from .models import Idea,Star,StarredIdeas,Profile,Comment
+from .permissions import IsUserChangingTheirOwnProfile,IsTheUserDoingActionsOnTheirOwnProfile
+from .serializers import IdeaSerialiser,StarSerialiser,ProfileSerialiser,UserSerialiser,CommentSerialiser
 # Create your views here.
 
 class IdeaViewSet(viewsets.ModelViewSet):
     queryset = Idea.objects.all()
     serializer_class = IdeaSerialiser
+    permission_classes = [IsTheUserDoingActionsOnTheirOwnProfile]
+
+class RandomIdeaViewSet(viewsets.ViewSet):
+    queryset = Idea.objects.all()
+    serializer_class = IdeaSerialiser
+    def get(self,request):
+        import ipdb;ipdb.set_trace()
+        queryset = random.choice(Idea.objects.all())
+        serializer_class = IdeaSerialiser(queryset)
+        return Response(serializer_class.data)
+
+
 
 class StarViewSet(viewsets.ViewSet):
     queryset = Star.objects.all()
     serializer_class = StarSerialiser
-    @staticmethod
-    def pk_correspands_to_an_idea_check(pk:int) -> [bool] or [bool,Response]:
-        pk_correspands_to_an_idea = bool(
-            Idea.objects.filter(
-                pk=pk
-            )
-        )
-        if not pk_correspands_to_an_idea:
-            return [False,Response(
-                {'status':'Article object with the given pk dosen\'t exist.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )]
-        return [True]
 
     @staticmethod
-    def star_object_exists_check(pk:int) -> [bool] or [bool,Response]:
-        not_empty = bool(Star.objects.filter(
+    def de_star(request,pk,idea_object):
+        import ipdb;ipdb.set_trace()
+        star_obj = Star.objects.get(
             related_idea=pk
-        ))
-        if not_empty:
-            return [True,Response(
-                {'status':'Can\'t create an already existing star object.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )]
-        return [True]
+        )
+        star_obj.stars -= 1
+        star_obj.save()
+        StarredIdeas.objects.get(
+            user=request.user.profile
+        ).delete()
+
+    def get(self,request,pk=None) -> Response:
+        queryset = Star.objects.all()
+        serializer = StarSerialiser(
+            queryset,
+            many=True,
+        )
+        return Response(serializer.data)
 
     def retrieve(self,request,pk=None) -> Response:
-        queryset = Star.objects.all()
-        star_object = get_object_or_404(queryset,pk=pk)
+        star_object = get_object_or_404(Star,pk=pk)
         serializer = StarSerialiser(star_object)
         return Response(serializer.data)
 
-    def create(self,request,pk=None) -> Response:
-        pk_correspands_to_an_idea = self.pk_correspands_to_an_idea_check(pk)
-        star_object_exists = self.star_object_exists_check(pk)
-        if not pk_correspands_to_an_idea[0]:
-            return pk_correspands_to_an_idea[1]
-        if star_object_exists[0]:
-            return star_object[1]
-        else:
-            queryset = Star.objects.all()
-            serializer = StarSerialiser(queryset,data=request.data,many=True)
-            star_obj = Star(stars=int(request.data['stars']),related_idea=Idea.objects.filter(pk=pk)[0])
+    def partial_update(self,request,pk=None):
+        star_object = get_object_or_404(
+            Star,
+            pk=pk,
+        )
+        idea_object = get_object_or_404(
+            Idea,
+            pk=pk,
+        )
+        serializer = StarSerialiser(
+            star_object,
+            data=request.data,
+            partial=True,
+            context={
+                'request':request,
+                'pk':pk,
+                'idea_object':idea_object,
+            }
+        )
+        import ipdb;ipdb.set_trace()
+        if serializer.is_valid(raise_exception=True):
+            import ipdb;ipdb.set_trace()
+            if serializer.validated_data.get('de_star'):
+                self.de_star(request,pk,idea_object)
+                return Response({'status':'decreamented by one.'})
+            #FIXME: Serializer returns old values, fix it
+            star_obj = Star(
+                id=pk,
+                stars=int(request.data['stars']),
+                related_idea=idea_object,
+            )
             star_obj.save()
-            return Response(serializer.data) if serializer.is_valid() else Response({})
+            starredidea_obj = StarredIdeas(
+                related_idea=idea_object,
+                user=request.user.profile
+            )
+            starredidea_obj.save()
+            return Response(
+                serializer.data
+            )
+        return Response({'status':'bad_request'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
-    def update(self,request,pk=None):
-        pass
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerialiser
+    permission_classes = [IsUserChangingTheirOwnProfile]
+    http_method_names = ['get','head','put','patch']
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerialiser
+    permission_classes = [IsTheUserDoingActionsOnTheirOwnProfile]
