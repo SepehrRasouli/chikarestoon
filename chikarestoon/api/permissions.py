9dab1d673d196578bc0ff39eae03445036adce1b
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-12 20:04:05 +0430

    Added permissions

---
+++ chikarestoon/api/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import BasePermission,SAFE_METHODS
+from django.contrib.auth.models import User
+class IsUserChangingTheirOwnProfile(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return bool(
+            request.user.is_authenticated and
+            request.user == obj.user or
+            request.user.is_staff
+        )
+
+class IsTheUserDoingActionsOnTheirOwnProfile(BasePermission):
+    def has_permission(self,request,view):
+        is_user_changing_his_own_post = True
+        if request.method == 'POST' and request.data != {}:
+            is_user_changing_his_own_post = request.user.pk == int(request.data['author'])
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user.is_authenticated and
+            is_user_changing_his_own_post
+        )
+
+
