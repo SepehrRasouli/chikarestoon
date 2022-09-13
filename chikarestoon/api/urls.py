917217d6cd950ead6232bc2e7620f074b1d154a8
SepehrRasouli <sepehrrs06@gmail.com>
2022-09-13 09:09:11 +0430

    Pylinted urls.py

--- chikarestoon/api/urls.py
+++ chikarestoon/api/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path,include
-from .views import IdeaViewSet,StarViewSet,ProfileViewSet,CommentViewSet,RandomIdeaViewSet
+from django.urls import path
 from rest_framework import routers
+from .views import IdeaViewSet,StarViewSet,ProfileViewSet,CommentViewSet,RandomIdeaViewSet
 router = routers.SimpleRouter()
 router.register(r'idea', IdeaViewSet,basename="idea")
 router.register(r'profile',ProfileViewSet,basename='profile')
 router.register('comment',CommentViewSet,basename='comment')
 urlpatterns = [
     path('star/',StarViewSet.as_view({'get':'get'})),
-    path('star/retrieve/<int:pk>/',StarViewSet.as_view({'get':'retrieve','patch':'partial_update'})),
+    path(
+        'star/retrieve/<int:pk>/',
+        StarViewSet.as_view({'get':'retrieve','patch':'partial_update'})),
     path('idea/random/',RandomIdeaViewSet.as_view({'get':'get'}))
 ]
 
